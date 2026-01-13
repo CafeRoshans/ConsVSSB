1f73bf06223fbe1a9c75054cb345cca8df39dac3
CafeRoshans <62197377+CafeRoshans@users.noreply.github.com>
2026-01-13 20:25:44 +0300

    6

--- ConsoleApplicationSkillbox.cpp
+++ ConsoleApplicationSkillbox.cpp
@@ -2,13 +2,13 @@
 #include <string>
 
 int main() {
-    std::string myString = "Hello, World!";
+    std::string myString = "";
     std::cout << "Str: " << myString << std::endl;
     std::cout << "Len str: " << myString.length() << " symb" << std::endl;
     
     if (!myString.empty()) {
-        std::cout << "First: " << myString[0] << std::endl;
-        std::cout << "Last: " << myString[myString.length() - 1] << std::endl;
+        std::cout << "First: " << myString.front() << std::endl;
+        std::cout << "Last: " << myString.back() << std::endl;
     } else {
         std::cout << "Is empty" << std::endl;
     }
