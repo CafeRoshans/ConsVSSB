1da177ac9e07e6f5b3da62b954f7bab049acf400
CafeRoshans <62197377+CafeRoshans@users.noreply.github.com>
2026-01-13 19:06:49 +0300

    1

---
+++ ConsoleApplicationSkillbox.cpp
@@ -0,0 +1,17 @@
+﻿// main.cpp
+#include <iostream>
+#include "Helpers.h"
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    // Вызов функции из Helpers.h
+    int result = squareOfSum(num1, num2);
+
+    std::cout << "Numm 1: " << num1 << std::endl;
+    std::cout << "Numm 2: " << num2 << std::endl;
+    std::cout << "Summ**2: " << result << std::endl;
+
+    return 0;
+}
---
+++ Helpers.h
@@ -0,0 +1,10 @@
+// Helpers.h
+#ifndef HELPERS_H
+#define HELPERS_H
+
+int squareOfSum(int a, int b) {
+    int sum = a + b;
+    return sum * sum;
+}
+
+#endif // HELPERS_H
