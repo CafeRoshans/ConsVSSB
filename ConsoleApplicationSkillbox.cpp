254866cac9a3a1bf22544d0b5670fe2ff2aa02f0
CafeRoshans <62197377+CafeRoshans@users.noreply.github.com>
2026-01-15 18:29:06 +0300

    56

--- ConsoleApplicationSkillbox.cpp
+++ ConsoleApplicationSkillbox.cpp
@@ -1,21 +1,35 @@
 ﻿#include <iostream>
-#include <string>
-
-void PrintEvenOrOddNumbers(int maxNumber, bool IsOdd) {
-    std::cout << "Numbers from 0 to " << maxNumber;
-    std::cout << (IsOdd ? " (Even): " : " (Odd): ");
-
-    for (int i = (IsOdd ? 0 : 1); i <= maxNumber; i += 2) {
-        std::cout << i << " ";
-    }
-    std::cout << std::endl;
-}
+#include <iomanip>
 
 int main() {
-    const int N = 20;
+    const int N = 5;
+    const int T = 13; // у меня 14 число на момент задания
 
-    PrintEvenOrOddNumbers(N, true);
-    PrintEvenOrOddNumbers(N, false);
-    
+    int matrix[N][N];
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            matrix[i][j] = i + j;
+        }
+    }
+
+    std::cout << "Matrix " << N << "x" << N << ":" << std::endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            std::cout << std::setw(2) << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+    int sum = 0;
+    for (int j = 0; j < N; j++) {
+        sum += matrix[T % N - 1][j];
+    }
+
+    std::cout << sum << std::endl;
+
+    std::cout << (T%N-2)*N+(N*(N+1))/2 << std::endl; // в одну строку через формулу
+
     return 0;
 }
